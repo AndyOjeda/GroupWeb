be231d181dbdb8c221eefef9468e9c38d5be9fb0
Valentina Martinez <valen282112@gmail.com>
2024-05-29 21:10:42 -0500

    navbar styles

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { AppComponent }  from '../app.component';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppComponent],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
