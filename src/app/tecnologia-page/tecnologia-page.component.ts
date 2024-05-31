5e8981e99727096f47377b91f7435548e8437759
vale282112 <valen282112@gmail.com>
2024-05-30 23:47:41 -0500

    Página Tecnologia

--- src/app/tecnologia-page/tecnologia-page.component.ts
+++ src/app/tecnologia-page/tecnologia-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-tecnologia-page',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './tecnologia-page.component.html',
   styleUrl: './tecnologia-page.component.css'
 })
