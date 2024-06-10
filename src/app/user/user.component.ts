00a974cb927ba9eb88801824485e7efcfcdf6ff0
vale282112 <valen282112@gmail.com>
2024-06-09 19:56:13 -0500

    userchanges

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [SidebarModule, ButtonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  sidebarVisible: boolean = false;
 }
