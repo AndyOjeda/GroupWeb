212b5208d64495853c4c77cfe68962ce30ee0629
Valentina Martinez <valen282112@gmail.com>
2024-06-12 18:31:45 -0500

    form changes

--- src/app/inicio-page/inicio-page.component.ts
+++ src/app/inicio-page/inicio-page.component.ts
@@ -6,19 +6,6 @@ import { NgModel } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { FooterComponent } from '../footer/footer.component';
 import { CarouselModule } from 'primeng/carousel';
-<<<<<<< HEAD
-import { NavigationComponent } from "../navigation/navigation.component";
-import { RouterOutlet } from '@angular/router';
-
-@Component({
-    selector: 'app-inicio-page',
-    standalone: true,
-    templateUrl: './inicio-page.component.html',
-    styleUrl: './inicio-page.component.css',
-    encapsulation: ViewEncapsulation.Emulated // Esto es por defecto
-    ,
-    imports: [CardModule, InputTextModule, FooterComponent, NavigationComponent, RouterOutlet]
-=======
 
 import { NavigationComponent } from '../navigation/navigation.component';
 
@@ -30,7 +17,6 @@ import { NavigationComponent } from '../navigation/navigation.component';
   styleUrl: './inicio-page.component.css',
   encapsulation: ViewEncapsulation.Emulated  // Esto es por defecto
 
->>>>>>> e19172f9f1e704b33c1a5496b131b37aaa555a83
 })
 export class InicioPageComponent {
 
