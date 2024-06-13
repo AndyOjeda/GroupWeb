4d381b5c325a267173871cd20b5e3dc30c8aecb3
Valentina Martinez <valen282112@gmail.com>
2024-06-13 00:32:30 -0500

    fix accordion

---
+++ src/app/product-page/product-page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GalleriaModule } from 'primeng/galleria';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GalleriaModule
+  ]
+})
+export class ProductPageModule { }
--- src/app/tecuser/tecuser.component.ts
+++ src/app/tecuser/tecuser.component.ts
@@ -4,16 +4,18 @@ import { SidebarModule } from "../sidebar/sidebar.module";
 import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { FormpopupModule } from '../formpopup/formpopup.module';
+import { DeletepopupModule } from "../deletepopup/deletepopup.module";
 
 @Component({
-  selector: 'app-tecuser',
-  standalone: true,
-  templateUrl: './tecuser.component.html',
-  styleUrls: ['./tecuser.component.css'],
-  imports: [NavuserComponent, SidebarModule, RouterOutlet, FormpopupModule]
+    selector: 'app-tecuser',
+    standalone: true,
+    templateUrl: './tecuser.component.html',
+    styleUrls: ['./tecuser.component.css'],
+    imports: [NavuserComponent, SidebarModule, RouterOutlet, FormpopupModule, DeletepopupModule]
 })
 export class TecuserComponent {
   isModalVisible = false;
+  isDeletePopupVisible = false;
 
   openModal() {
     this.isModalVisible = true;
@@ -21,5 +23,20 @@ export class TecuserComponent {
 
   closeModal() {
     this.isModalVisible = false;
+  }
+
+  showDeletePopup() {
+    this.isDeletePopupVisible = true;
+  }
+
+  closeDeletePopup() {
+    this.isDeletePopupVisible = false;
+  }
+
+  confirmDelete() {
+    this.isDeletePopupVisible = false;
+    // Lógica para cerrar sesión
+    console.log('Elemento Eliminado');
+    // Aquí puedes redirigir al usuario a la página de inicio de sesión o realizar otras acciones necesarias.
   }
 }
