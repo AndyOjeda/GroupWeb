c1221aacf13b25a88f555f17daa723869fd0ee9a
Valentina Martinez <valen282112@gmail.com>
2024-06-17 21:41:31 -0500

    Pop Up Finished

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,28 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
   isCollapsed = false;
-  isLogoutPopupVisible = false;
+  Deletevisible: boolean = false;
+  visible: boolean = false;
+
 
   toggleSidebar() {
     this.isCollapsed = !this.isCollapsed;
   }
 
-  showLogoutPopup() {
-    this.isLogoutPopupVisible = true;
+  showDeleteDialog() {  
+    this.Deletevisible = true;
   }
 
-  closeLogoutPopup() {
-    this.isLogoutPopupVisible = false;
-  }
-
-  confirmLogout() {
-    this.isLogoutPopupVisible = false;
-    // Lógica para cerrar sesión
-    console.log('Sesión cerrada');
-    // Aquí puedes redirigir al usuario a la página de inicio de sesión o realizar otras acciones necesarias.
+  showDialog() {
+    this.visible = true;
   }
 }
