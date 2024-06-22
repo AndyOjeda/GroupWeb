f26fcf9fb9a9b7459840dc0197c98ee7a151885d
3lPr1mo <erickenriquechaparro@gmail.com>
2024-06-21 21:02:25 -0500

    fix: navigate to info product from home

--- src/app/inicio-page/inicio-page.component.ts
+++ src/app/inicio-page/inicio-page.component.ts
@@ -1,16 +1,12 @@
 import { Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import { Router } from '@angular/router';
-import { PrimeNGConfig } from 'primeng/api';
 import { CardModule } from 'primeng/card';
-import { NgModel } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { FooterComponent } from '../footer/footer.component';
 import { CarouselModule } from 'primeng/carousel';
-
 import { NavigationComponent } from '../navigation/navigation.component';
 import { ApiService } from '../Services/api.service';
 import { ProductResponse } from './dto/InicioDTOs';
-import { error } from 'console';
 
 @Component({
   selector: 'app-inicio-page',
@@ -26,7 +22,7 @@ export class InicioPageComponent implements OnInit{
 
   @Input() data: ProductResponse[] | null = null;
 
-  constructor(private readonly service: ApiService){}
+  constructor(private readonly service: ApiService, private router: Router){}
   
   ngOnInit(): void {
     this.service.getProductsByDivision().subscribe({
@@ -50,6 +46,12 @@ export class InicioPageComponent implements OnInit{
 
   abrirCorreo() {
     window.open('mailto:Group_ms@outlook.com');
+  }
+
+  navigateToProductInfo(id: number) {
+    this.router.navigate(['product/',id]).then(() => {
+      window.location.reload()
+    })
   }
 
 }
